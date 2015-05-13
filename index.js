8ed1d5fd9369b6b1786c1e10b88491a17a6da696
Tom Wilson <tom@jackhq.com>
2015-05-12 21:32:00 -0400

    fixed bugs

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ var request = require('request')
 
 module.exports = function (ee) {
 	// add scheduled task
-	ee.on('request-scheduler-create', function(event) {
+	ee.on('request-scheduler-create', function (event) {
 		// validate object
 		// add to schedule
-	  new CronJob(event.object.cron, sendRequest(event.object.request))
+	  new CronJob(event.object.cron, 
+		  sendRequest(event.object.request), 
+			function () { console.log('done') }, true)
+		
 	})
 
   // execute task
 	function sendRequest(obj) {
 		return function() {
-			request[object.method](object.url, {
-				body: object.body,
+			request(obj.url, {
+				method: obj.method,
+				body: obj.body,
 				headers: {
 					'Content-Type': obj.mimeType
 				}
 			}, function (e, r, b) {
-				ee.emit('request-response', {
-					model: 'request',
+				ee.emit('request-scheduler-response', {
+					model: 'request-scheduler',
 					verb: 'response',
 					object: b 
 				})			
